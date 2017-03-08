cbac004e38ab33b83300b1e868f2ea49142dc082
George Seeger <george@polylytics.com>
2017-03-08 15:42:53 +0000

    Created user model

---
+++ InstagramClone/Models/Domain/User.cs
@@ -0,0 +1,9 @@
+﻿namespace InstagramClone.Models.Domain
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
