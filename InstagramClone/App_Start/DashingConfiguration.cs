e79e19ccfc8556019a9075758b88736cf1d653ac
George Seeger <george@polylytics.com>
2017-03-08 15:42:39 +0000

    Dashing connection and config

---
+++ InstagramClone/App_Start/DashingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Dashing.Configuration;
+using InstagramClone.Models.Domain;
+
+namespace InstagramClone.App_Start
+{
+    public class DashingConfiguration : DefaultConfiguration
+    {
+        public DashingConfiguration() : base(ConfigurationManager.ConnectionStrings["DefaultConnection"])
+        {
+            this.AddNamespaceOf<User>();
+        }
+    }
+}
