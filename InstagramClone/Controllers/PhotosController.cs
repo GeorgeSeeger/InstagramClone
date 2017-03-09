cf355480ce3967e516e853eeb58fb1cee7b248f3
George Seeger <george@polylytics.com>
2017-03-09 08:49:56 +0000

    Submit form and basic controller created for Photos

---
+++ InstagramClone/Controllers/PhotosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InstagramClone.Controllers
+{
+    public class PhotosController : Controller
+    {
+        // GET: Photos
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Create()
+        {
+//            var photo = new Photo();
+            return View();
+        }
+    }
+}
